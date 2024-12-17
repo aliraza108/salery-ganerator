52a9a5d4bf21f665eb18e71cfc48a25771813d43
Ahmed-Ali-Abro <aliraza476265@gmail.com>
2024-12-17 11:16:16 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# name = input("what is your name")
+# attendance = int(input("what is your attendance: "))
+# total_salary = int(input("what is your total salery: "))
+# overtime = int(input("how much hours over time you work: "))
+# total_days = 30
+
+# # assinging per day pay and overtimes
+# overtime_perday = overtime / 8
+# absent =  total_days- attendance 
+# perday_pay = total_salary/total_days
+# total_payment = (total_days + overtime_perday) * perday_pay
+# print(f"{name} your total absent is {absent} and your total payment is {total_payment}")
+
+def func(a,b):
+    print(a)
+    print(b)
+    print(int(a) + int(b))
+func(6,"05")
